2cd96d8a3c11a4760be89536df815e4d3e88690d
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2021-11-20 21:17:04 -0800

    Add spec 3, -WIP

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Expected Output: [0]
 Test: "It should return an string that starts with 0 and ends with the inputted number"
 Code: beebBoop(5);
 Expected Output: "0, 1, 2, 3, 4, 5"
+
+Test: "If a number contains 1, it should return "Beep!"
+Code: beepBoop(5);
+Expected Output: "0, Beep!, 2, 3, 4 , 5"
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
+// function beepBoop(number) {
+//   let input = number;
+//   let index = 0
+//   let inputArray = [];
+  
+//   while
+//   (index <= input) {
+//   inputArray.push(index);
+//   index ++
+//   }
+// return inputArray;
+// }
+
 function beepBoop(number) {
   let input = number;
   let index = 0
   let inputArray = [];
-  
+  let beepArray = new Array(input);
   while
   (index <= input) {
   inputArray.push(index);
   index ++
   }
-  let output = inputArray.toString();
-  return output;
+  inputArray.forEach(function(element) {
+  if (element.toString().includes(1)) {
+  beepArray.push("Beep!");
+  }
+  else {
+  beepArray.push(inputArray[index]);
+  }
+  return beepArray;
+});
 }
 
 
